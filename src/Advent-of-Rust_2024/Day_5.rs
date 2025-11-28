2ebd4a739120e0ec825686a9a7df32e9e0bf0695
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-11-28 12:47:36 +0800

    feat(day_5): finished day 5 exercise.

---
+++ src/Advent-of-Rust_2024/Day_5.rs
@@ -0,0 +1,69 @@
+pub const GOOD_WEIGHT: f32 = 1.0;
+pub const BAD_WEIGHT: f32 = 2.0;
+
+#[derive(Debug, PartialEq)]
+pub enum Niceness {
+    Nice(u32),
+    Naughty,
+}
+
+pub struct Kid {
+    pub name: String,
+    pub niceness: Niceness,
+}
+
+impl Kid {
+    pub fn parse_row(csv_row: &str) -> Result<Kid, &'static str> {
+        // 🎁 Transform the CSV row into a Kid struct for Santa's list!
+        // 🎅 Expected CSV: "Name,GoodDeeds,BadDeeds"
+        //    Example: "Alice,3,1" -> name: "Alice", good_deeds: 3, bad_deeds: 1
+
+        // 🎁 Your code here! 🎁
+
+        let mut arr = csv_row.split(',');
+
+        let name = match arr.next() {
+            Some(name) => String::from(name),
+            None => return Err("No name"),
+        };
+
+        let good_deeds: u32 = match arr.next() {
+            Some(g) => g.parse().map_err(|_| "Parse gooddeeds error")?,
+            None => return Err("No gooddeeds"),
+        };
+
+        let bad_deeds: u32 = match arr.next() {
+            Some(b) => b.parse().map_err(|_| "Parse baddeeds error")?,
+            None => return Err("No baddeeds"),
+        };
+
+        Ok(Self::new(name, good_deeds, bad_deeds))
+    }
+
+    pub fn new(name: String, good_deeds: u32, bad_deeds: u32) -> Self {
+        let niceness = if Self::is_nice(good_deeds, bad_deeds) {
+            Niceness::Nice(good_deeds)
+        } else {
+            Niceness::Naughty
+        };
+
+        Self { name, niceness }
+    }
+
+    pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
+        if good_deeds == 0 && bad_deeds == 0 {
+            return false;
+        }
+
+        let good_deeds = good_deeds as f32 * GOOD_WEIGHT;
+        let bad_deeds = bad_deeds as f32 * BAD_WEIGHT;
+
+        let ratio = good_deeds / (good_deeds + bad_deeds);
+
+        ratio >= 0.75
+    }
+}
+
+fn main() {
+    println!("Hello world");
+}
